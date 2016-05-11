6a846bf7bb90f92924e4ea569d97f31fa08a4a4a
panpan7 <873046130@qq.com>
2016-05-11 12:43:22 +0800

    早睡早起

---
+++ 11/hello.js
@@ -0,0 +1,41 @@
+// alert("Hi");
+
+
+// dfdfdf
+
+// dfdfd
+
+// dfdfd
+
+
+
+
+// var 性别 = '女';
+// alert(性别);
+
+var age = 90;
+// alert(20 == '20');
+
+// {
+//     alert(age + 3);
+//     alert("13" + "广告");
+// }
+
+if (age > 40) {
+    if (age >= 90) { alert("早睡早起"); } else {
+        alert("锻炼身体");
+    }
+} else {
+    alert("人生的路还很长");
+}
+
+for (var i = 0; i < 100; i++) {
+    console.log(i)
+        // 在控制台输出信息
+}
+
+function sayHello(name) {
+    console.log(name + ", 你好");
+}
+
+sayHello("早安喵");
