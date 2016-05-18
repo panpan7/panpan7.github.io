330808727114a88c0be889d41fdb6d3610f01677
panpan7 <873046130@qq.com>
2016-05-18 12:44:21 +0800

    xin

---
+++ 12/bom.js
@@ -0,0 +1,44 @@
+// 对象的声明及使用
+
+var person ={
+	name:'ight',
+	gender:'male',
+	age:20,
+	sing:function() {
+		console.log('正在唱歌');
+	}
+}
+
+//console.log(person.name); 
+//person.sing();
+//for (var i in person){
+//  console.log(i + ":
+
+
+
+for (var i in console){
+	console.log(i +";"+console[i]);
+}
+console.clear();
+
+console.log(typeof person);
+
+//布尔类型
+
+console.log(typeof false);
+console.log(typeof 'false');
+
+//数字类型
+console.log(typeof 100);
+
+
+//字符串类型
+console.log(typeof person.name);
+
+//定义数组
+var sites = ['baidu.com','sohu.com','sina.com.cn','163.'];
+
+console.log(sites[0]);
+
+console.log(typeof sites);
+
